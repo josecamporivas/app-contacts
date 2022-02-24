573f11aa344269a19991a71ff423f3e671eb40fc
Jose Campo Rivas <josecampor31@gmail.com>
2022-02-24 13:32:58 +0100

    add some logic work
    add to script.js some actions when sign up

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,22 @@
+const form = document.getElementById("form");
 
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const userName = event.target.name.value;
+    const userPassword = event.target.password.value;
+
+    if(userCorrect(userName, userPassword)){
+        //go to contacts.html (with contacts of the user)
+    }else{
+        //show message error
+    }
+})
+
+
+function userCorrect(userName, userPassword){
+    if(!userName || !userPassword){
+        return false;
+    }
+    
+    //conect to the api to validate the user (connecting to the database)
+}
